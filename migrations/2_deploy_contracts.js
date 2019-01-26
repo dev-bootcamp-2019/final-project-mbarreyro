a3c89742be6b8a81ba31828aa85968a62c6ffde7
Martín Ernesto Barreyro <martin.barreyro@scalablepath.com>
2019-01-26 19:51:02 -0300

    Add emergency stoppable tests

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,11 @@
 var Marketplace = artifacts.require("./Marketplace.sol");
 var Administrable = artifacts.require("./Administrable.sol");
+var EmergencyStoppable = artifacts.require("./EmergencyStoppable.sol");
 
 module.exports = function(deployer, network, accounts) {
   const admins = [accounts[0], accounts[1]];
 
   deployer.deploy(Marketplace, admins);
   deployer.deploy(Administrable, admins);
+  deployer.deploy(EmergencyStoppable, admins);
 };
---
+++ test/emergencyStoppable.js
@@ -0,0 +1,58 @@
+const EmergencyStoppable = artifacts.require("./EmergencyStoppable.sol");
+
+contract("EmergencyStoppable", accounts => {
+  const admin1 = accounts[0];
+  const externalUser = accounts[6];
+
+  it("...can only be stopped by admins and NOT by external users", async () => {
+    /*
+     * Try to stop the contract with an admin account and an external account
+     */
+    const emergencyStoppableInstance = await EmergencyStoppable.deployed();
+    let failed = false;
+
+    try {
+      await emergencyStoppableInstance.stop.call({ from: admin1 });
+    } catch (e) {
+      // this should never happen
+      failed = true;
+    }
+
+    assert.isFalse(failed, 'Stop call from an admin should not fail');
+
+    try {
+      await emergencyStoppableInstance.stop.call({ from: externalUser });
+    } catch (e) {
+      // this should always happen
+      failed = true;
+    }
+
+    assert.isTrue(failed, 'Stop call from an external user should fail');
+  });
+
+  it("...can only be started by admins and NOT by external users", async () => {
+    /*
+     * Try to start the contract with an admin account and an external account
+     */
+    const emergencyStoppableInstance = await EmergencyStoppable.deployed();
+    let failed = false;
+
+    try {
+      await emergencyStoppableInstance.start.call({ from: admin1 });
+    } catch (e) {
+      // this should never happen
+      failed = true;
+    }
+
+    assert.isFalse(failed, 'Start call from an admin should not fail');
+
+    try {
+      await emergencyStoppableInstance.start.call({ from: externalUser });
+    } catch (e) {
+      // this should always happen
+      failed = true;
+    }
+
+    assert.isTrue(failed, 'Start call from an external user should fail');
+  });
+});
